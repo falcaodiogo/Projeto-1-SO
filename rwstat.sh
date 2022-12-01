ef13b1f49956a8aba91f267d0b0f02da1574cc01
zegameiro <jose.mcgameiro@ua.pt>
2022-12-01 14:49:02 +0000

    Options fully functional except for -r and - w

--- rwstat.sh
+++ rwstat.sh
@@ -20,8 +20,7 @@ declare -a information
 
 # Variáveis globais
 numProcesses="null"     # Número de processos  
-reverse=0               # Encontra-se desativado, caso o utilizador ative, esta variável passa a ter o valor 1
-order=0                 # Por defeito, a ordenação é feita por ordem alfabética
+reverse=1               # Por defeito, a ordenação é feita por ordem decrescente dos valores de rater
 comm_opt=". *"          # Caso o utilizador não insira nenhum argumento do tipo '-c' irá guardar todos os processos
 user_opt="*"                # Caso o utilizador não insira nenhum argumento do tipo '-u' irá guardar todos os utilizadores
 start=$(date +%s)              # Guarda a data de início da execução do script
@@ -36,80 +35,6 @@ if [ "$seconds" != "${@: -1}" ]; then
     echo "ERRO: O argumento do tipo inteiro e positivo tem de ser o último"
     exit 1
 fi
-
-while getopts ":c:s:e:u:m:M:p:rw" opt; do   # Percorrer todos os argumentos
-    case $opt in
-        c)
-            comm_opt=$OPTARG                  # Guarda o comando
-            option_c="-c"
-        ;;
-
-        s)  
-            # Data mínima
-            # Guarda a data mínima em segundos
-            option_s="-s"
-            min_Date=$(date -d "$OPTARG" +%s)
-            if [[ -z "$min_Date" ]]; then
-                echo "ERRO: A data mínima não é válida"
-                exit 
-            fi
-        
-        ;;
-
-        e)
-            option_e="-e"                 # Guarda a data de fim
-            max_Date=$(date -d "$OPTARG" +%s)      # Guarda a data de fim em segundos
-        ;;
-
-        u)
-            user_opt=$OPTARG                   # Guarda o utilizador
-            option_u="-u"
-
-        ;;
-
-        m)
-            minPID=$OPTARG                 # Guarda o PID mínimo
-            option="-m"
-
-        ;;
-
-        M)
-            if ! [[ $OPTARG =~ $rexp ]] ; then       # Verifica do argumento, este tem de ser um número inteiro positivo
-                echo "Erro!! O argumento do '-M' tem de ser um número positivo!!"
-                exit 1
-            fi
-            maxPID=$OPTARG                 # Guarda o PID máximo
-
-        ;;
-
-        p)
-            numProcesses=$OPTARG           # Guarda o número de processos
-            #verifica se existem 2 númereos 
-            if [ $# -lt 2 ]; then
-                echo "Erro!! O argumento do '-p' tem de ser um número positivo!!"
-                exit 1
-            fi
-            if ! [[ $numProcesses =~ $rexp ]] ; then       # Verifica do argumento, este tem de ser um número inteiro positivo
-                echo "Erro!! O argumento do '-p' tem de ser um número positivo!!"
-                exit 1
-            fi
-        ;;
-
-        r)
-            reverse=1                      # Ativa a ordenação reversa
-        ;;
-
-        w)
-            order=1                        # Ativa a ordenação por write values
-
-        ;;
-
-        *)
-            echo "Erro!! Argumento(s) inválido(s)!!"
-            exit 1
-        ;;
-    esac
-done
 
 sleep $seconds
 count=0
@@ -151,8 +76,7 @@ for pid in $(ps -eo pid | tail -n +2); do   # Percorre todos os processos
                 # start_time
                 
                 sdate=$(ps -p $pid -o lstart | tail -n +2)
-                dates_seconds1[$count]=$(date -d"$sdate" +%s) # Guarda a data em segundos
-                dates_seconds2[$count]=$(date -d"$sdate" +%s) # Guarda a data em segundos
+                dates_seconds[$count]=$(date -d"$sdate" +%s) # Guarda a data em segundos
                 date=$(date -d "$(ps -p $pid -o lstart | tail -1 | awk '{print $1, $2, $3, $4}')" +"%b %d %H:%M" )
 
                 # Adicionar a um array as informações todas
@@ -164,75 +88,110 @@ for pid in $(ps -eo pid | tail -n +2); do   # Percorre todos os processos
 done
 # ------------------------------------------------------------------------------------------
 
-# Opção -c
-if [[ $option_c -eq "-c" ]] ; then
-    for i in "${!process_info[@]}" ; do
-        aux=${process_info[i]}
-        commands=${aux[0]}   # Vai buscar o comando
-        first_char=${commands:0:1}
-        # Comparação de um comando com uma expressão regular
-		if [[ ! $first_char =~ $comm_opt ]] ; then    # Irá retirar todos os processos(mais as suas informações) que forem diferentes da expressão regular que o utilizador inseriu 
-            unset process_info[$i]
-        fi
-    done
-fi
+while getopts ":c:s:e:u:m:M:p:rw" opt; do   # Percorrer todos os argumentos
+    case $opt in
+        c)  # Opção -c
+            comm_opt=$OPTARG       # Guarda o comando inserido pelo utilizador
+            for i in "${!process_info[@]}" ; do
+                aux=${process_info[i]}
+                commands=${aux[0]}   # Vai buscar o comando
+                first_char=${commands:0:1}
+                # Comparação de um comando com uma expressão regular
+		        if [[ ! $first_char =~ $comm_opt ]] ; then    # Irá retirar todos os processos(mais as suas informações) que forem diferentes da expressão regular que o utilizador inseriu 
+                    unset process_info[$i]
+                fi
+            done
+        ;;
 
-# # Opção -u
-if [[ $option_u -eq "-u" ]] ; then
-    for i in "${!process_info[@]}" ; do
-        aux=(${process_info[i]})
-        user=${aux[1]}
-        if [[ $user != $user_opt ]] ; then
-            unset process_info[$i]
-        fi
-    done
-fi
+        u)  # Opção -u
+            user_opt=$OPTARG       # Guarda o utilizador inserido
+            for i in "${!process_info[@]}" ; do
+                aux=(${process_info[i]})
+                user=${aux[1]}
+                if [[ $user != $user_opt ]] ; then
+                    unset process_info[$i]
+                fi
+            done
+        ;;
 
-# Opção -s 
-if [[ $option_s -eq "-s" ]] ; then
-    for i in "${!dates_seconds1[@]}" ; do
-        if [[ ${dates_seconds1[i]} -lt $min_Date ]] ; then
-            unset process_info[$i]
-        fi
-    done
-fi
+        s)  # Opção -s
+            # Guarda a data mínima em segundos inserida pelo utilizador
+            min_Date=$(date -d "$OPTARG" +%s)
+            if [[ -z "$min_Date" ]]; then
+                echo "ERRO: A data mínima não é válida"
+                exit 
+            fi
+            for i in "${!dates_seconds[@]}" ; do
+                if [[ ${dates_seconds[i]} -lt $min_Date ]] ; then
+                   unset process_info[$i]
+                fi
+            done
+        
+        ;;
 
-# Opção -e
-if [[ $option_e -eq "-e" ]] ; then
-    for i in "${!dates_seconds2[@]}" ; do
-        if [[ ${dates_seconds2[i]} -gt $max_Date ]] ; then
-            unset process_info[$i]
-        fi
-    done
-fi
+        e)  # Opção -e
+            max_Date=$(date -d "$OPTARG" +%s)  # Guarda a data de fim em segundos inserida pelo utilizador
+            for i in "${!dates_seconds[@]}" ; do
+                if [[ ${dates_seconds[i]} -gt $max_Date ]] ; then
+                    unset process_info[$i]
+                fi
+            done
+        ;;
 
-# Opção -m
-# if [[ $option -eq "-m" ]] ; then
-#     echo "${process_info[2]}"
-#     for i in "${!process_info[@]}" ; do
-#         aux=(${process_info[i]})
-#         pids=$aux[2]
-#         if [[ ${pids[i]} -gt $minPID ]] ; then
-#             unset process_info[$i]
-#         fi
-#     done
-# fi
+        m)  # Opção -m
+            minPID=$OPTARG      # Guarda o PID mínimo inserido pelo utilizador
+            for i in "${!process_info[@]}" ; do
+                aux=(${process_info[i]})
+                pid=${aux[2]}
+                if [[ $pid -lt $minPID ]] ; then
+                    unset process_info[$i]
+                fi
+            done
 
-# Opção -M
-# if [[ $option -eq "-M" ]] ; then
-#     for i in "${!processID[@]}"; do
-#         if [[ ${processID[i]} -lt $maxPID ]]; then
-#             unset comm[i]
-#             unset user[i]
-#             unset processID[i]
-#             unset rchar_array[i]
-#             unset wchar_array[i]
-#             unset rater_array[i]
-#             unset ratew_array[i]
-#             unset dates[i]
-#         fi
-#     done
-# fi
+
+        ;;
+
+        M)  # Opção -M
+            maxPID=$OPTARG   # Guarda o PID máximo inserido pelo utilizador
+            for i in "${!process_info[@]}" ; do
+                aux=(${process_info[i]})
+                pid=${aux[2]}
+                if [[ $pid -gt $maxPID ]] ; then
+                    unset process_info[$i]
+                fi
+            done
+
+        ;;
+
+        p)
+            numProcesses=$OPTARG           # Guarda o número de processos
+            #verifica se existem 2 númereos 
+            if [ $# -lt 2 ]; then
+                echo "Erro!! O argumento do '-p' tem de ser um número positivo!!"
+                exit 1
+            fi
+            if ! [[ $numProcesses =~ $rexp ]] ; then       # Verifica do argumento, este tem de ser um número inteiro positivo
+                echo "Erro!! O argumento do '-p' tem de ser um número positivo!!"
+                exit 1
+            fi
+        ;;
+
+        r)
+            reverse=1                      # Ativa a ordenação reversa
+        ;;
+
+        w)
+            reverse=0
+            order=1                        # Ativa a ordenação por write values
+
+        ;;
+
+        *)
+            echo "ERRO: Argumento(s) inválido(s)!!"
+            exit 1
+        ;;
+    esac
+done
 
 max=$(($count))
 
@@ -241,7 +200,18 @@ if [[ $numProcesses != 0 ]] ; then
     printf "%-40s %-20s %-10s %-20s %-10s %-15s %-15s %-10s \n" "COMM" "USER" "PID" "READB" "WRITEB" "RATER" "RATEW" "DATE"  # Impressão do cabeçalho
     for ((i=0; i<=$max; i++)) ; do
         # se o array for null, não imprime nada
+        # descending order of rater
+        
         information=${process_info[i]}
+        if [[ $reverse == 1 ]] ; then
+            printf "%-40s %-20s %-10s %-20s %-10s %-15s %-15s %-10s \n" ${information[0]} ${information[1]} ${information[2]} ${information[3]} ${information[4]} ${information[5]} ${information[6]} ${information[7]} | sort -n -k 6
+        else
+            if [[ $order == 1 ]] ; then
+                printf "%-40s %-20s %-10s %-20s %-10s %-15s %-15s %-10s \n" ${process_info[$i]} | sort -n -k 6
+            else
+                printf "%-40s %-20s %-10s %-20s %-10s %-15s %-15s %-10s \n" ${process_info[$i]} | sort -n -k 5
+            fi
+        fi
         if [[ ${information[0]} == "" ]]; then
             continue
         fi
